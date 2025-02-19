3c8bb3825c70400ddbddd62d4523724408da3752
005LuxmiSaini <luxmisaini005@gmail.com>
2025-02-19 22:13:13 +0530

    change code

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ if (js ==="amazing")alert("Javascript is FUN");
 
 46+8+48-48;
 console.log(46+8+48-48);
+console.log("luxmi");
+console.log(46);
+//Variables and Values
+let firstName = "Luxmi";
+let lastName = "Saini";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName + " " + lastName);
+
+let age = 19;
+let PI = 3.1455;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Student";
+
+let job1 = "programmer";
+let job2 = "student";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+console.log(job1);
+console.log(job2);
+
