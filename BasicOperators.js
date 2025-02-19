4754a609c37d1dc52cf9a978e2c5bc8ebb55f61c
005LuxmiSaini <luxmisaini005@gmail.com>
2025-02-19 21:58:25 +0530

    add new file

---
+++ BasicOperators.js
@@ -0,0 +1,24 @@
+//Basic Operators
+var year, yearLuxmi, yearMuskan;
+var now = 2025;
+ageLuxmi = 19;
+ageMuskan = 18;
+//Math Operators
+var yearLuxmi = now - ageLuxmi;
+var yearMuskan = now - ageMuskan;
+console.log(yearLuxmi);
+
+console.log(now + 2);
+console.log(now + 2);
+console.log(now / 2);
+
+//Logical Operators
+var luxmiOlder = ageLuxmi > ageMuskan;
+console.log(luxmiOlder);
+
+//type of operator
+console.log(typeof luxmiOlder);
+console.log(typeof ageLuxmi);
+console.log(typeof "Muskan is older the Luxmi");
+var x;
+console.log(typeof x);
