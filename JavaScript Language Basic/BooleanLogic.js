f6c195892717b004a6d14060aa160fc76b2807ef
005LuxmiSaini <luxmisaini005@gmail.com>
2025-02-20 14:19:29 +0530

    change folder

---
+++ JavaScript Language Basic/BooleanLogic.js
@@ -0,0 +1,9 @@
+var firstName = "Luxmi";
+var age = 16;
+ if (age < 13){
+    console.log(firstName + " is a girl.");
+ }else if (age >= 13 && age < 20){
+    console.log(firstName + " is a teenager.");
+ }else {
+    console.log(firstName + " is a woman.");
+ }
