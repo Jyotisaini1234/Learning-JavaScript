66f79b2b968131536db2fae403d1ea983634fd0a
005LuxmiSaini <luxmisaini005@gmail.com>
2025-02-22 13:01:29 +0530

    function

---
+++ JavaScript Language Basic/Function.js
@@ -0,0 +1,30 @@
+// Functions
+
+
+function calculateAge(birthYear){
+    return 2025 - birthYear;
+}
+ 
+var ageLuxmi = calculateAge(2006);
+var ageLux = calculateAge(2005);
+var ageman = calculateAge(1958);
+var ageaman = calculateAge(2000);
+console.log(ageLuxmi, ageLux, ageman, ageaman);
+
+
+function yearUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0){
+        console.log(firstName + " retires in " + retirement + " years.");
+    } else {
+        console.log(firstName + " is already retired.");
+    }
+    
+}
+
+yearUntilRetirement(2006, "Luxmi");
+yearUntilRetirement(2005, "Lux");
+yearUntilRetirement(1958, "man");
+yearUntilRetirement(2000, "aman");
