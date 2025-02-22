82e225badfff40902e731bc4b06554bdb8f239b5
005LuxmiSaini <luxmisaini005@gmail.com>
2025-02-22 19:57:41 +0530

    chanfe file

---
+++ JavaScript Language Basic/Function StatementandExpressions.js
@@ -0,0 +1,22 @@
+//Function Statement and Expressions
+
+//Function declaration 
+// function whatDoYouDo(job, firstName){}
+
+// Function expression
+var whatDoYouDo = function(job, firstName){
+    switch(job){
+        case "teacher":
+          return firstName + " teaches kids how to code ";
+        case "driver":
+            return firstName + " drives a cab in India.";
+        case "designer":
+            return firstName + " designs beautiful websites.";
+        default:
+            return firstName + " anything else.";
+    }
+}
+
+console.log(whatDoYouDo("designer", "Jhon"));
+console.log(whatDoYouDo("teacher", "Mark"));
+console.log(whatDoYouDo("driver", "Luxmi"));
--- JavaScript Language Basic/Function.js
+++ JavaScript Language Basic/Function.js
@@ -36,3 +36,4 @@ function myFunction(){
 
 myFunction();
 myFunction();
+
