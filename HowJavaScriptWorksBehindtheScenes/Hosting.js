8401878e3feb11a511360bb51ab7485d0494a993
005LuxmiSaini <luxmisaini005@gmail.com>
2025-02-24 23:12:06 +0530

    Hosting

---
+++ HowJavaScriptWorksBehindtheScenes/Hosting.js
@@ -0,0 +1,31 @@
+
+
+/// Hosting
+
+//function
+calculateAge(2006);
+
+ function calculateAge(year){
+    console.log(2025 - year);
+ }
+
+
+ //retirement(2006);
+
+ var retirement =  function(year){
+    console.log(60 - (2025 - year));
+ }
+
+ //variables
+ console.log(age);
+ var age = 19;
+ console.log(age);
+
+ function foo(){
+    console.log(age);
+    var age = 13;
+    console.log(age);
+ }
+
+ foo();
+ console.log(age);
