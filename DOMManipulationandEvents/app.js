4a5b1a1cec164417c35b6175422d9918b32b0f7b
005LuxmiSaini <luxmisaini005@gmail.com>
2025-03-02 16:13:19 +0530

    modified

--- DOMManipulationandEvents/app.js
+++ DOMManipulationandEvents/app.js
@@ -2,8 +2,20 @@ var scores, roundscore, activePlayer, dice;
 
 scores = [0,0];
 roundscore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current' + activePlayer).textContent = dice;
+document.querySelector('#current' + activePlayer).textContent = "dice";
+//document.querySelector('#current' + activPlayer).innerHTML = '<em>' + dice + '<em>';
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').styele.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+}
+
