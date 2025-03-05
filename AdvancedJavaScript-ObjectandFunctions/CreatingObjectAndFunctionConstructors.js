c2183a8501b35e7dc0f84dbabe0390a1f050fa50
005LuxmiSaini <luxmisaini005@gmail.com>
2025-03-05 15:12:11 +0530

    CreatingObjectAndFunctionConstrustor

---
+++ AdvancedJavaScript-ObjectandFunctions/CreatingObjectAndFunctionConstructors.js
@@ -0,0 +1,34 @@
+// function conatructor
+
+ var jhon = {
+    name: 'Jhon',
+    yearOfBirth: 1998,
+    job: 'teacher'
+ };
+
+ 
+ var Person = function(name,yearOfBirth,job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+   
+ }
+
+Person.prototype.calculateAge =
+function() {
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = "Smith";
+ var jhon = new Person('Jhon',1998,'teacher');
+var jane = new Person('Jane',1969,'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+jhon.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(jhon.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+console.log(lux.lastName);
