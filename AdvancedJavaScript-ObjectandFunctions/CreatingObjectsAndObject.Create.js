89edde73878d416caff17b4e40f70172ceb59c72
005LuxmiSaini <luxmisaini005@gmail.com>
2025-03-05 20:11:25 +0530

    index.html

---
+++ AdvancedJavaScript-ObjectandFunctions/CreatingObjectsAndObject.Create.js
@@ -0,0 +1,22 @@
+// Objects.Create
+
+var  personProto = {
+    calculateAge: function(){
+        console.log(2016 - this.yearOfBirth); 
+      }
+};
+
+var jhon = Object.create(personProto);
+jhon.name = 'Jhon';
+jhon.yearOfBirth =1998;
+jhon.job = 'teacher';
+
+var jane = Object.create(personProto,
+    {
+        name:{ value: 'Jane'},
+        yearOfBirth: { value: 1969},
+        job: { value: 'designer'}
+    }
+);
+
+
