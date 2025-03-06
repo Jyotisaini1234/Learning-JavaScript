b2f9165a92c7ac7269da1090fdc1b54efc99c1f9
005LuxmiSaini <luxmisaini005@gmail.com>
2025-03-06 21:07:54 +0530

    modified

--- AdvancedJavaScript-ObjectandFunctions/ImmediatelyInvokedFunctionExpressions.js
+++ AdvancedJavaScript-ObjectandFunctions/ImmediatelyInvokedFunctionExpressions.js
@@ -1,1 +1,19 @@
-r
+// Immediately Invoked Function Expressions
+
+function game(){
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+
+(function () {
+    var score = Math.random()* 10;
+    console.log(score >= 5);
+})();
+
+//console.log(score);
+
+(function (goodLuck) {
+    var score = Math.random()* 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
