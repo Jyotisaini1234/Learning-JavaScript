4f9c0f826a3aa811b93274e7af4e8d54693adbc5
005LuxmiSaini <luxmisaini005@gmail.com>
2025-03-20 22:23:56 +0530

    add file

--- TheBudgetAppProject/Budget.js
+++ TheBudgetAppProject/Budget.js
@@ -16,7 +16,26 @@ var UIController = (function(){
 // Global APP Controller
 var controller = (function(budgetCtrl,UICtrl){
 
-  document.querySelector('add_btn').addEventListener('click',function(){
-    console.log('Button was click');
-  })
+   var ctrlAddItem = function(){
+      //1. Get the filed input data
+
+    //2. Add the item to the budget controller
+
+    //3. Add the item to the UI
+
+    //4. Calculate the budget
+
+    //5. Display the budget on the UI
+
+       console.log('It works ');
+   }
+
+  document.querySelector('.add_btn').addEventListener('click',ctrlAddItem());
+  
+  document.addEventListener('keypress',function(event){
+    
+     if (event.keycode === 13 || event.which ===13){
+      ctrlAddItem();
+     }
+  });
 })(budgetController,UIController);
