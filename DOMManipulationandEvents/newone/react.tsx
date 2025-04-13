d711d009d80e2051df04d44daa4d2b114adaef91
jyotisainii <jyotisaini5614@gmail.com>
2025-04-13 22:14:52 +0530

    newone

---
+++ DOMManipulationandEvents/newone/react.tsx
@@ -0,0 +1,6 @@
+
+export const BASE_URL = 'http://ec2-35-154-100-109.ap-south-1.compute.amazonaws.com:8081'; 
+export const GENERATEUID=`${BASE_URL}/sightTour/generateUid`;
+export const SIGHTTOURBYCITY =`${BASE_URL}/sightTour/byCity`;
+export const ACTIVITIESBYSIGHTCODE =`${BASE_URL}/sightTour/activities/bySightCode`;
+export const HOTELDETAILS=`${BASE_URL}/sightTour/hotelbyCity`
